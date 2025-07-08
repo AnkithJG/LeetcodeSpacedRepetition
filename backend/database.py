e3b8f60da44063add3a7ddac2b6b529748f49392
Ankith Ganji <ganjiankith@gmail.com>
2025-07-08 15:31:23 -0500

    supabase setup

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,3 @@
-
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from fastapi import HTTPException
@@ -10,17 +9,20 @@ def get_db_cursor():
     conn = None
     try:
         conn = psycopg2.connect(
-            host="localhost",
-            database="RepeetCode",
+            host="db.igbrtovzwzggsshovtak.supabase.co",
+            database="postgres",
             user="postgres",
-            password=os.getenv("DB_PASS"),
-            cursor_factory=RealDictCursor  # Returns rows as dicts
+            password=os.getenv("DB_PASS"),  
+            port=5432,
+            cursor_factory=RealDictCursor
         )
         cursor = conn.cursor()
         yield cursor
         conn.commit()
     except Exception as e:
-        if conn: conn.rollback()
+        if conn:
+            conn.rollback()
         raise HTTPException(500, f"Database error: {str(e)}")
     finally:
-        if conn: conn.close()
+        if conn:
+            conn.close()
